25abc6b8c6582a1040a6c9dcaf217130234de0bc
Imane Ouchrif <imane.ouchrif91@gmail.com>
2024-06-06 13:19:31 +0200

    add function getCategories

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -18,8 +18,13 @@ let categories = [];
 // TODO : créer une fonction getWorks et une fonction getCategories
 
 const getWorks = async () => {
-    const response = await fetch ("http://localhost:5678/api/works");
+    const response = await fetch("http://localhost:5678/api/works");
     getWorks = await response.json();
+};
+
+const getCategories = async () => {
+    const response = await fetch("https://localhost:5678/api/catgories");
+    getCategories = await response.json();
 };
 
 // *********************** MAIN *********************** //
