82ebc351a1f874a70e2a801ea6298d359f45b096
Imane Ouchrif <imane.ouchrif91@gmail.com>
2024-06-06 15:53:05 +0200

    improve fetch

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -4,9 +4,9 @@
 
 const URL = "http://localhost:5678/api/";
 
-const mainGallery   = document.querySelector(".gallery");
-const projectTitle  = document.querySelector("#portfolio h2");
-const loginLink     = document.querySelector("nav :nth-child(3)");
+const mainGallery = document.querySelector(".gallery");
+const projectTitle = document.querySelector("#portfolio h2");
+const loginLink = document.querySelector("nav :nth-child(3)");
 
 // *********************** VARIABLES *********************** //
 
@@ -15,20 +15,26 @@ let categories = [];
 
 // *********************** FUNCTIONS *********************** //
 
-// TODO : créer une fonction getWorks et une fonction getCategories
-
 const getWorks = async () => {
-    const response = await fetch("http://localhost:5678/api/works");
-    allWorks = await response.json();
-};
+  const response = await fetch(URL + "works");
+
+  return await response.json();
+}
 
 const getCategories = async () => {
-    const response = await fetch("https://localhost:5678/api/catgories");
-    allCategories = await response.json();
-};
+  const response = await fetch(URL + "categories");
+
+  return await response.json();
+}
+
+const getData = async () => {
+  works = await getWorks();
+  categories = await getCategories();
+} 
 
 // *********************** MAIN *********************** //
 
-console.log("mainGallery : ", typeof mainGallery, mainGallery);
-console.log("projectTitle : ", typeof projectTitle, projectTitle);
-console.log("loginLink : ", typeof loginLink, loginLink);
+getData();
+
+console.log("works : ", typeof works, works);
+console.log("categories : ", typeof categories, categories);
