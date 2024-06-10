16909a552a4a93e475e04eccc85cb8ceee3022ac
Imane Ouchrif <imane.ouchrif91@gmail.com>
2024-06-10 11:53:55 +0200

    add constants login/email/password login.js

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -4,4 +4,7 @@
 
 const LOGIN_URL = "http://localhost:5678/api/users/login";
 
+const login     = document.getElementById("login")
+const email     = document.getElementById("email") 
+const password  = document.getElementById("password")
 
