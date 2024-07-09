538da81780456d313325432ab08ed25fc4eea154
Imane Ouchrif <imane.ouchrif91@gmail.com>
2024-07-09 20:29:07 +0200

    js inject gallery

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -6,6 +6,7 @@ const URL = "http://localhost:5678/api/";
 
 const mainGallery = document.querySelector(".gallery");
 const projectTitle = document.querySelector("#portfolio h2");
+const filters = document.querySelector(".filters");
 const loginLink = document.querySelector("nav :nth-child(3)");
 
 // *********************** VARIABLES *********************** //
@@ -18,23 +19,63 @@ let categories = [];
 const getWorks = async () => {
   const response = await fetch(URL + "works");
 
-  return await response.json();
+  works = await response.json();
 }
 
 const getCategories = async () => {
   const response = await fetch(URL + "categories");
 
-  return await response.json();
+  categories = await response.json();
 }
 
+const displayWorks = () => {
+  for (const work of works) {
+    console.log(work)
+
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
+
+    figure.id = work.categoryId;
+    img.src = work.imageUrl;
+    img.alt = work.title;
+    figcaption.innerText = work.title;
+
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    mainGallery.appendChild(figure);
+  }
+}
+
+const displayFilters = () => {
+  const all = document.createElement("button");
+  const allItem = document.createElement("li");
+
+  all.id = "all";
+  all.innerText = "Tous";
+
+  allItem.appendChild(all);
+  filters.appendChild(allItem);
+
+  for (const category of categories) {
+    const button = document.createElement("button");
+    const li = document.createElement("li");
+
+    button.id = category.id;
+    button.innerText = category.name;
+
+    li.appendChild(button);
+    filters.appendChild(li);
+  }
+}
 const getData = async () => {
-  works = await getWorks();
-  categories = await getCategories();
+  await getWorks();
+  await getCategories();
+
+  displayWorks();
+  displayFilters();
 } 
 
 // *********************** MAIN *********************** //
 
 getData();
-
-console.log("works : ", typeof works, works);
-console.log("categories : ", typeof categories, categories);
